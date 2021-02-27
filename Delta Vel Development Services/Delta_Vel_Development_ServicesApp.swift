e0a732ce24fbea334157a95f204af9e93ffb290f
Alexander Lester <alexander480@gmail.com>
2021-02-27 01:03:49 -0500

    Initial Commit

---
+++ Delta Vel Development Services/Delta_Vel_Development_ServicesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Delta_Vel_Development_ServicesApp.swift
+//  Delta Vel Development Services
+//
+//  Created by Alexander Lester on 2/27/21.
+//
+
+import SwiftUI
+
+@main
+struct Delta_Vel_Development_ServicesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
